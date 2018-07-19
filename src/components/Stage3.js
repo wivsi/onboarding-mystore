fc2e33d346ab0d5b44539d95f9b47b8eb3cbb088
William <william.v.siva@gmail.com>
2018-07-19 14:20:53 +0200

    dont know

---
+++ src/components/Stage3.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import HeaderText from './HeaderText';
+
+class Stage3 extends React.Component{    
+    render(){
+        return(
+            <div>
+                <HeaderText headerTitle={"Creating your store..."} headerSub={"Please select what type of store you want below"} /> 
+            </div>
+        )
+    }
+}
+
+export default Stage3;
