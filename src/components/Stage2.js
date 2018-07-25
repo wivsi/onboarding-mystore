a7764c12b9adba0a3c557184c01a82314e6d1216
William <william.v.siva@gmail.com>
2018-07-25 15:48:38 +0200

    redirecting from stage2->stage3 on right sms-code

--- src/components/Stage2.js
+++ src/components/Stage2.js
@@ -1,39 +1,44 @@
-import React from 'react';
-import { withRouter
-  } from 'react-router-dom'
+import React from "react";
+import { withRouter } from "react-router-dom";
 
-import HeaderText from './HeaderText';
-import VerificationForm from './VerificationForm';
+import HeaderText from "./HeaderText";
+import VerificationForm from "./VerificationForm";
 
-const queryString = require('query-string');
+const queryString = require("query-string");
+const generatedCode = 2345;
 
-class Stage2 extends React.Component{
-    state  = {
-        sms_code: "",
-        email: "",
-        phone: ""
-    };
+class Stage2 extends React.Component {
+  state = {
+    sms_code: "",
+    email: "",
+    phone: ""
+  };
 
-    onSubmit = sms_code => {
-        this.setState({sms_code})
+  onSubmit = sms_code => {
+    this.setState({ sms_code });
 
-        // Check here if the code is equal to the generated code
-        // If the code is equal -> Redirect client to Stage3 and generate a demo for the client
+    if (sms_code == generatedCode) {
+      this.props.history.push({
+        pathname: "/Stage3"
+      });
+    } else {
+        console.log("Clear all input fields and give out error message...");
     }
+  };
 
-    render(){
-        return(
-            <div>
-                <HeaderText headerTitle={"Verification"} headerSub={"Please enter the four digits to activate your account"}/>
-                <VerificationForm onSubmit={code => this.onSubmit(code)} /> 
-                <p>You entered code: {(this.state.sms_code)}</p>
-                {
-                    console.log(queryString.parse(this.props.location.search))
-                    
-                }
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <HeaderText
+          headerTitle={"Verification"}
+          headerSub={"Please enter the four digits to activate your account"}
+        />
+        <VerificationForm onSubmit={code => this.onSubmit(code)} />
+        <p>You entered code: {this.state.sms_code}</p>
+        {console.log(queryString.parse(this.props.location.search))}
+      </div>
+    );
+  }
 }
 
 export default withRouter(Stage2);
