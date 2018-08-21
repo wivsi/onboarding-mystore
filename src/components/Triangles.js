c4aed3a16993b68d04ad9e909716167cd1da0432
William <william.v.siva@gmail.com>
2018-08-21 09:08:19 +0200

    ..

---
+++ src/components/Triangles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import triangle1 from './../assets/svg/Path202.svg';
+
+
+class Triangles extends React.Component{    
+    render(){
+        return(
+            <div>
+                <img src={triangle1}/>
+            </div>
+        )
+    }
+}
+
+export default Triangles;
